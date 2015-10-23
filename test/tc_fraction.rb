9a428e35a7d54adcde405d777b88c6a60dc361ca
alu0100825985 <alu0100825985@ull.edu.es>
2015-10-23 22:05:17 +0100

    Prueba 2 de la resta realizada correctamente y sin errores

--- test/tc_fraction.rb
+++ test/tc_fraction.rb
@@ -11,5 +11,6 @@ class TestFraction < Test::Unit::TestCase
 
    	def test_substraction # Pruebas unitarias para la resta.
    		assert_equal([1,4], Fraction.new(2,4).substraction(1,4)) 
+   		assert_equal([11,20], Fraction.new(3,4).substraction(1,5))
    	end
 end
