f7860ec3551ed06f682497883e3d22faaa61b710
alu0100825985 <alu0100825985@ull.edu.es>
2015-10-23 21:34:57 +0100

    Prueba 2 de la suma realizada correctamente y sin errores

--- test/tc_fraction.rb
+++ test/tc_fraction.rb
@@ -4,6 +4,7 @@ require 'test/unit'
 class Testfraction < Test::Unit::TestCase
 
    def test_add
-      assert_equal([1,2], Fraction.new(1,4).add(1,4))       
+      assert_equal([1,2], Fraction.new(1,4).add(1,4)) 
+      assert_equal([19,20], Fraction.new(3,4).add(1,5))         
    end
 end
