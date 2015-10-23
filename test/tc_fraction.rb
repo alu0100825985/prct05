509c51d5a11175ae18153ef0b0b7287ee33d88f1
alu0100825985 <alu0100825985@ull.edu.es>
2015-10-23 21:36:51 +0100

    Prueba 3 de la suma realizada correctamente y sin errores

--- test/tc_fraction.rb
+++ test/tc_fraction.rb
@@ -1,10 +1,11 @@
-require "./lib/fraction.rb"
+require "./lib/fraction.rb" # Añadimos el fichero fraction.rb con nuestra clase de operaciones aritméticas con fracciones.
 require 'test/unit'
 
 class Testfraction < Test::Unit::TestCase
 
-   def test_add
+   def test_add # Pruebas unitarias para la suma.
       assert_equal([1,2], Fraction.new(1,4).add(1,4)) 
-      assert_equal([19,20], Fraction.new(3,4).add(1,5))         
+      assert_equal([19,20], Fraction.new(3,4).add(1,5)) 
+      assert_equal([169,50], Fraction.new(47,25).add(15,10))
    end
 end
